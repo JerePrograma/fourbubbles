be79fb124914fa802b7b6c4d70a97e5128747a55
Jeremías <95102411+JerePrograma@users.noreply.github.com>
2026-07-20 14:29:35 -0300

    feat: route client editing and order creation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,10 @@ import { ProtectedRoute } from './components/ProtectedRoute';
 import { AgendaPage } from './pages/AgendaPage';
 import { ClientsPage } from './pages/ClientsPage';
 import { DashboardPage } from './pages/DashboardPage';
+import { EditClientPage } from './pages/EditClientPage';
 import { LoginPage } from './pages/LoginPage';
 import { NewClientPage } from './pages/NewClientPage';
+import { NewOrderPage } from './pages/NewOrderPage';
 import { OrdersPage } from './pages/OrdersPage';
 
 export default function App(): JSX.Element {
@@ -17,7 +19,9 @@ export default function App(): JSX.Element {
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/clients" element={<ClientsPage />} />
           <Route path="/clients/new" element={<NewClientPage />} />
+          <Route path="/clients/:id/edit" element={<EditClientPage />} />
           <Route path="/orders" element={<OrdersPage />} />
+          <Route path="/orders/new" element={<NewOrderPage />} />
           <Route path="/agenda" element={<AgendaPage />} />
         </Route>
       </Route>
