ab3bf1d8b34471726fc9fc2233f62752ea77fb0b
Jeremías <95102411+JerePrograma@users.noreply.github.com>
2026-07-20 18:02:40 -0300

    feat: support versioned compatibility lookup

--- backend/src/main/java/ar/com/ropalista/compatibility/persistence/CompatibilityEvaluationRepository.java
+++ backend/src/main/java/ar/com/ropalista/compatibility/persistence/CompatibilityEvaluationRepository.java
@@ -2,8 +2,10 @@ package ar.com.ropalista.compatibility.persistence;
 
 import ar.com.ropalista.compatibility.domain.CompatibilityEvaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface CompatibilityEvaluationRepository extends JpaRepository<CompatibilityEvaluation, UUID> {
+public interface CompatibilityEvaluationRepository extends JpaRepository<CompatibilityEvaluation, UUID>,
+        JpaSpecificationExecutor<CompatibilityEvaluation> {
 }
