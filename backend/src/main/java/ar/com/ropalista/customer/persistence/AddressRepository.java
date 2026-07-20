af88fd75ef2220f86f582e8c706761102aa63142
Jeremías <95102411+JerePrograma@users.noreply.github.com>
2026-07-20 15:00:45 -0300

    feat: query active and historical addresses

--- backend/src/main/java/ar/com/ropalista/customer/persistence/AddressRepository.java
+++ backend/src/main/java/ar/com/ropalista/customer/persistence/AddressRepository.java
@@ -3,9 +3,13 @@ package ar.com.ropalista.customer.persistence;
 import ar.com.ropalista.customer.domain.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface AddressRepository extends JpaRepository<Address, UUID> {
     Optional<Address> findByIdAndClientIdAndActiveTrue(UUID id, UUID clientId);
+    List<Address> findByClientIdAndActiveTrueOrderByPrimaryAddressDescValidFromDesc(UUID clientId);
+    List<Address> findByClientIdAndActiveFalseOrderByValidToDesc(UUID clientId);
+    long countByClientIdAndActiveTrue(UUID clientId);
 }
